aee0478ab69e8e5c6a65289e86a7ec0b8f8d9eaa
Heath Blackmon <coleoguy@gmail.com>
2022-05-27 12:01:07 -0500

    Update testing-HB.R

--- annabel/testing-HB.R
+++ annabel/testing-HB.R
@@ -4,11 +4,11 @@ source("functions.R")
 # list
 runs <- 5
 pop_size <- 100
-gen_no <- 1000
-mu <- 0.00000001
+gen_no <- 300
+mu <- 1^-6
 fus.type <- "Y"
 fus.large <- F
-s <- 1
+s <- .3
 # Get probabilities of 0, 1, 2, and 3 mutations in a generation
 
 
@@ -16,42 +16,20 @@ pop <- GetPop(pop_size)
 
 # Get a distribution of fitness effects
 dfe <- GetDFE()
-
-# Initialize list of matrices
-# For each matrix...
-# Rows = Generations
-# Columns = Individuals
-results <- matrix(nrow = 4*gen_no, ncol = pop_size,
-                  dimnames = list(
-                    rep(c("X-SDR", "X/Y-SDR", "X-FuseLocus", "X/Y-FuseLocus"), 
-                        gen_no), 1:pop_size))
-
-# Create iterator for the FOUR rows corresponding to the current generation
-gen_rows <- 1:4
-
-# For each generation in "gen_no"...
+mon <- c()
 for(gen in 1:gen_no){
   print(paste(c("Generation: ", gen), collapse = ""))
-  
   # Mutate the starting population
-  pop <- ActofGod(pop, dfe, fus.type, mu.table, fus.large)
-  
+  pop <- ActofGod(pop, dfe, fus.type, mu, fus.large)
   # Assess the fitness of the mutated population
   fits <- GetFit(pop, s)
+  mon[gen] <- mean(fits)
   # Add loci and fitnesses to corresponding generation in output
-  for(i in 1:pop_size){
-    results[gen_rows, i] <- c(pop[[i]][,13], pop[[i]][,25])
-  }
-  
   # Select parents based on the fitness
   parents <- Maury(pop, fits, length(pop))
-  
   # Select gametes from these parents
   gametes <- MakeGametes(pop, parents, chiasm=T)
-  
   # Breed the parents
   pop <- MiracleOfLife(gametes)
-  
   # Move to next two generation's rows
-  gen_rows <- gen_rows + 4
 }
