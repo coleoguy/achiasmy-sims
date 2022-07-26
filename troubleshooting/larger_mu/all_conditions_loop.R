cb9b945c31cc1baad950361075318de63fbf555f
Heath <knightoftheperrytable@gmail.com>
2022-07-26 13:12:19 -0500

    results of running Y-small simulations on 3 mutation rates

--- troubleshooting/larger_mu/all_conditions_loop.R
+++ troubleshooting/larger_mu/all_conditions_loop.R
@@ -3,7 +3,7 @@
 # This script loops through the simulation with all possible combinations of
 # meiotic & fusion parameters as well as all selection coefficients from 0:1
 
-source("functions.R")
+source("../../scripts/functions.R")
 
 #################### Create Cluster for Parallel Processing #################### 
 sim_cluster <- makeCluster(spec = 100, type = "SOCK")
@@ -13,48 +13,30 @@ registerDoSNOW(cl = sim_cluster)
 num_sims <- 1000
 gen_no <- 1000
 pop_size <- 1000     
-# Selection coefficients ranging from 0 (negative control) to 1 (most extreme)
-s_coeffs <- c(0.25,0.3,0.35)
+# Run at s of 0.1, because at this selection coefficient SOME Y-S simulations
+# will fix but not all will fix
+s <- 0.1
 # Empirically-derived mutation rate for Drosophila
-mu <- 5e-9
-# There are 8 possible combinations of parameters because there are 2 values 
-# each for "chiasm", "fus.type",  and "fus.size"source("functions.R")
+mus <- c(5e-8, 5e-7, 5e-6)
 
-num_conditions <- 8 
+# To start, run only on CYS and AYS
+meiotic_types <- c("C", "A")
+fus.type <- "Y"
+fus.large <- F
+size <- "S"
+# Run first sim chiasmatic and second achiasmatic
+chiasm <- T
 
-# Run with each possible combination of parameters. 
-for(i in 1:length(s_coeffs)){
-  s <- s_coeffs[i]a
-  for(cond in 1:num_conditions){ 
+# Run with each possible combination of mu and chiasmy
+for(i in 1:length(mus)){
+  mu <- mus[i]
+  for(cond in 1:length(meiotic_types)){ 
     time.start <- Sys.time()
-    # Run first 4 sims chiasmatic and final four achiasmatic
-    if(cond <= 4){
-      chiasm <- T
-      meiotic_type <- "C"
-    }else{
-      chiasm <- F 
-      meiotic_type <- "A" 
-    }
-    
-    # Every 2 runs, switch fusing from Y to X
-    if(cond %in% c(1, 2, 5, 6)){
-      fus.type <- "Y"
-    }else{
-      fus.type <- "X"
-    } 
-    
-    # Every other run, switch from small to large
-    if(cond%%2){
-      fus.large <- F
-      size <- "S"
-    }else{
-      fus.large <- T
-      size <- "L"
-    } 
+    meiotic_type <- meiotic_types[cond]
     
     # Prep filename based on current parameters 
-    filename <- paste(c("../results/", meiotic_type, fus.type, size, "_s=", s, 
-                         "_", as.character(Sys.Date()), ".rds"), collapse = "")
+    filename <- paste(c(meiotic_type, fus.type, size, "_mu=", mu,".rds"), 
+                      collapse = "")
     print(filename)
     
     # Create a list where each entry is a vector of frequencies for each 
@@ -64,10 +46,12 @@ for(i in 1:length(s_coeffs)){
     dfe <- GetDFE()
     fusion_frequencies <- foreach(sim = 1:num_sims) %dopar% {
       Evolve(pop_size, gen_no, s, chiasm, fus.type,  mu, fus.large, dfe)
-    }
+    } 
     saveRDS(fusion_frequencies, filename)
     time.elapsed <- Sys.time() - time.start
     print(time.elapsed)
+    # Run first sim chiasmatic and second achiasmatic
+    chiasm <- F
   }
 }
 
